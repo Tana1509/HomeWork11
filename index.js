5b3396ec7ae634ee8dc5a1ef218b591bfb00adc7
Tana1509 <tana1509@gmail.com>
2020-04-14 06:27:09 +0300

    add js

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // nav
+    let menuBtn = $(".header__burger");
+    console.log(menuBtn);
+    menuBtn.on("click", function () {
+        let navigation = $(".header__nav__list");
+        // console.log(navigation);
+        navigation.toggleClass("active");
+    });
+
+    
+});
